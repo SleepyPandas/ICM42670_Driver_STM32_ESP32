905edac48f23c10431d9eeef94531ecc1752ca7d
SleepyPandas <tommyrobotics1@gmail.com>
2026-04-27 20:55:58 -0400

    add Device Address for I2C either is 0x68 or 0x69 based on useer Config

--- ICM42670_Driver/ports/stm32_hal/ICM42670_stm32_hal.h
+++ ICM42670_Driver/ports/stm32_hal/ICM42670_stm32_hal.h
@@ -20,17 +20,17 @@ typedef struct {
   uint32_t timeout_ms;
 } ICM42670_STM32_SPIBus;
 
+ICM42670_Status_t ICM42670_STM32_SPI_INIT(ICM42670_Config *config,
+                                          ICM42670_STM32_SPIBus *bus,
+                                          SPI_HandleTypeDef *hspi,
+                                          GPIO_TypeDef *cs_port,
+                                          uint16_t cs_pin);
 
-ICM42670_Status_t ICM42670_STM32_SPI_INIT(
-    ICM42670_Config *config, ICM42670_STM32_SPIBus *bus,
-    SPI_HandleTypeDef *hspi, GPIO_TypeDef *cs_port, uint16_t cs_pin);
-
-int8_t ICM42670_STM32_SPI_ReadReg(void *handle, uint8_t reg_addr,
-                                  uint8_t *data, uint16_t len);
+int8_t ICM42670_STM32_SPI_ReadReg(void *handle, uint8_t reg_addr, uint8_t *data,
+                                  uint16_t len);
 
 int8_t ICM42670_STM32_SPI_WriteReg(void *handle, uint8_t reg_addr,
                                    const uint8_t *data, uint16_t len);
-
 
 typedef struct {
   // need SCLK and SDA which is in hi2c
@@ -38,22 +38,16 @@ typedef struct {
   uint32_t timeout_ms;
 } ICM42670_STM32_I2CBus;
 
+ICM42670_Status_t ICM42670_STM32_I2C_INIT(ICM42670_Config *config,
+                                          ICM42670_STM32_I2CBus *bus,
+                                          I2C_HandleTypeDef *hi2c);
 
-ICM42670_Status_t ICM42670_STM32_I2C_INIT(
-    ICM42670_Config *config, ICM42670_STM32_I2CBus *bus,
-    I2C_HandleTypeDef *hi2c);
-
-int8_t ICM42670_STM32_I2C_ReadReg(void *handle, uint8_t reg_addr,
-                                  uint8_t *data, uint16_t len);
+int8_t ICM42670_STM32_I2C_ReadReg(void *handle, uint8_t reg_addr, uint8_t *data,
+                                  uint16_t len);
 
 int8_t ICM42670_STM32_I2C_WriteReg(void *handle, uint8_t reg_addr,
-                                    const uint8_t *data, uint16_t len);
-
-
-
-
-
-
+                                   const uint8_t *data, uint16_t len,
+                                   uint8_t device_addr);
 
 #ifdef __cplusplus
 }
